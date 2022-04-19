aaf4e504956b973169f1f45af549acb37e2b2b18
Mariia Dryha <m.pishchanska@gmail.com>
2022-04-19 20:44:37 +0300

    add simplelightbox

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -2,17 +2,18 @@
 import axios from 'axios';
 
 axios.defaults.baseURL = 'https://pixabay.com/api/';
-axios.defaults.headers.post['Content-Type'] ='application/json; charset=utf-8';
+axios.defaults.headers.post['Content-Type'] = 'application/json; charset=utf-8';
 
- export function fetchData(data) {
-
-
-    const KEY = '26749427-98c3432f7608211f00519cfab';
-    const MAIN_CONFIG = 'image_type=photo&orientation=horizontal&safesearch=true';
-    const PAGE_CONFIG = 'per_page=40';
     
- return axios.get(`?key=${KEY}&q=${data}&${MAIN_CONFIG}&${PAGE_CONFIG}`);
 
+export function fetchData(item, page) {
+
+const KEY = '26749427-98c3432f7608211f00519cfab';
+const MAIN_CONFIG = 'image_type=photo&orientation=horizontal&safesearch=true';
+const PAGE_CONFIG = `per_page=100&page=${page}`;
+
+   return axios.get(`?key=${KEY}&q=${item}&${MAIN_CONFIG}&${PAGE_CONFIG}`);
+     
 }
            
 
